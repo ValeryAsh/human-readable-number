b3242369ae9b8be07dc29d7b645ddcce26142417
Valery <106538990+ValeryAsh@users.noreply.github.com>
2023-01-03 10:57:04 +0400

    feat: add main part code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function toReadable (number) {
-  
+  module.exports = function toReadable (number) {
+    const units = ['','one','two','three','four', 'five','six','seven','eight','nine'];
+    const twenty = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
+    const dozens = ['','','twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+    let a = number.toString().split('')
+    let result = []
+      if(a.length == 1){
+          result.push(units[number])
+          return result
+      }
+      if(a.length == 2){
+          if(a[0] > 1){
+              return dozens[+a[0]] + ' ' + units[+a[1]]
+          }
+          console.log(twenty[+a[1]])
+          return twenty[+a[1]]
+      }
+      if(a.length == 3){
+        if(a[0] >= 1){
+            result.push(units[+a[0]] + ' hundred ')
+        }
+        if(a[1] == 0){
+            result.push(
+                units[a[2]]
+            )
+        }
+        if( a[1] == 1){
+            result.push(twenty[a[1]])
+        }
+        if(a[1] > 1){
+            result.push(dozens[+a[1]] + ' ' + units[+a[2]])
+        } 
+
+        console.log(result.join(''))
+        return  result.join('')
+    }
+}
 }
